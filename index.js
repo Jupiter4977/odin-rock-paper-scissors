69d20f17800b4c71eac80e22ef119eec92883715
Cameron Brown <jupiter49777@gmail.com>
2025-12-15 19:23:39 +0200

    feat: Add getHumanChoice function to prompt players for a player move

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ function getComputerChoice() {
     return 'paper';
   } else return 'scissors';
 }
+
+function getHumanChoice() {
+  const userInput = prompt('Insert move: Rock Paper Scissors');
+  const humanChoice = userInput
+    ? userInput.toLowerCase().replace(/\s+/g, '')
+    : null;
+  switch (humanChoice) {
+    case null:
+      alert('Please insert a move');
+      return getHumanChoice();
+    case 'rock':
+      return humanChoice;
+    case 'paper':
+      return humanChoice;
+    case 'scissors':
+      return humanChoice;
+    default:
+      alert(
+        `"${humanChoice}" is not a valid move. \nValid moves: Rock Paper Scissors`
+      );
+      return getHumanChoice();
+  }
+}
